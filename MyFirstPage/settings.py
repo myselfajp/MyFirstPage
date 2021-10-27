061ade6d170eb841ee6d55ec51b4a75363f85d7e
myselfajp <myjaliliparsa@gmail.com>
2021-10-27 21:07:58 +0300

    aded statics and media
    + static url
    + static root
    + media url
    + media root
    and move satic directory to main page

--- MyFirstPage/settings.py
+++ MyFirstPage/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'MyFirstPage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
