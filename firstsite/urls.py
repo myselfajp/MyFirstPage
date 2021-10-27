6a68a579f0d264d525531ca94346bb479cdb1a24
myselfajp <myjaliliparsa@gmail.com>
2021-10-28 01:07:53 +0300

    Crated my first site designed
    add templates and synced statics with them.

--- firstsite/urls.py
+++ firstsite/urls.py
@@ -1,8 +1,12 @@
-from .views import http_about, http_contact, http_home
+from .views import http_about, http_blog_home, http_blog_single, http_contact, http_elements, http_home
 from django.urls import path
 
 urlpatterns = [
+    path('index.html',http_home),
     path('',http_home),
-    path("about",http_about),
-    path("contact",http_contact)
+    path("about.html",http_about),
+    path("contact.html",http_contact),
+    path("blog-home.html",http_blog_home),
+    path("blog-single.html",http_blog_single),
+    path("elements.html",http_elements)
 ]
--- firstsite/views.py
+++ firstsite/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 
 def http_home(request):
-    return render(request,"firstsite\Home.html")
+    return render(request,"firstsite\Index.html")
     
 def http_contact(request):
     return render(request,"firstsite\Contact.html")
 
-
 def http_about(request):
     return render(request,"firstsite\About.html")
+
+def http_blog_home(request):
+    return render(request,"firstsite\Blog-home.html")
+
+def http_blog_single(request):
+    return render(request,"firstsite\Blog-single.html")
+
+def http_elements(request):
+    return render(request,"firstsite\Elements.html")
+
+
+
+
 
