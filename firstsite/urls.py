99d8f147fabc752ff394478e666247efb889034a
myselfajp <myjaliliparsa@gmail.com>
2021-10-29 21:17:00 +0300

    template inheritance
    make a base.html file for all page
    and make urls dynamic

--- firstsite/urls.py
+++ firstsite/urls.py
@@ -1,12 +1,14 @@
 from .views import http_about, http_blog_home, http_blog_single, http_contact, http_elements, http_home
 from django.urls import path
 
+app_name="firstsite"
+
 urlpatterns = [
-    path('index.html',http_home),
+    path('index',http_home,name='index'),
     path('',http_home),
-    path("about.html",http_about),
-    path("contact.html",http_contact),
-    path("blog-home.html",http_blog_home),
-    path("blog-single.html",http_blog_single),
-    path("elements.html",http_elements)
+    path("about",http_about,name='about'),
+    path("contact",http_contact,name='contact'),
+    path("blog-home",http_blog_home,name='blog-home'),
+    path("blog-single",http_blog_single,name='blog-single'),
+    path("elements",http_elements,name='elements')
 ]
