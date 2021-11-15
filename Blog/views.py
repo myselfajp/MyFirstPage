f0d44e5559c58beb15e57a227bfa1b0590af0e6c
myselfajp <myjaliliparsa@gmail.com>
2021-11-15 17:40:08 +0300

    fix bug
    filter showen page by status on

--- Blog/views.py
+++ Blog/views.py
@@ -1,8 +1,9 @@
 from django.http import request
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from Blog.models import Post
 from requests import get
 from datetime import datetime
+
 
 # Create your views here.
 def http_blog_home(request):
@@ -11,21 +12,25 @@ def http_blog_home(request):
     get_API=get("http://worldtimeapi.org/api/timezone/Etc/UTC").json()['datetime'][2:19]
     utc_time_now=datetime.strptime(get_API, '%y-%m-%dT%H:%M:%S')
 
-    #make posts list with filter of published_date
-    posts=Post.objects.exclude(published_date__gt= utc_time_now)
+    #make posts list with filter of published_date and activated status
+    posts=Post.objects.exclude(published_date__gt= utc_time_now).filter(status=1)
+
     context={"posts":posts}
 
     return render(request,"Blog\Blog-home.html",context)
 
 
+
+
 def http_blog_single(request,p1_id):
 
-    posts=Post.objects.get(id=p1_id)
+    posts=get_object_or_404(Post ,id=p1_id,status=1)
     posts.counted_views+=1
     posts.save()
     context={"posts":posts}
     return render(request,"Blog\Blog-single.html",context)
     
+
 
 
 def http_test(request,p_id):
