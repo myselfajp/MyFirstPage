dace0400ff36c8715cccde1062c959dd02ecf123
myselfajp <myjaliliparsa@gmail.com>
2021-11-16 16:13:08 +0300

    Next and Prev Page area
    make next and previous page button in single page

--- Blog/views.py
+++ Blog/views.py
@@ -20,16 +20,42 @@ def http_blog_home(request):
 
 def http_blog_single(request,p1_id):
     
+
     posts=get_object_or_404(Post ,id=p1_id,status=1)
     posts.counted_views+=1
     posts.save()
-    context={"posts":posts}
+
+    utc_time_now=datetime.now(timezone.utc)
+    posts_np=Post.objects.exclude(published_date__gt= utc_time_now).filter(status=1)
+
+    for i in range(len(posts_np)):
+
+        if posts_np[i].id==posts.id:
+
+            try:
+                prev_p=posts_np[i-1]
+            except:
+                prev_p=''
+
+            try:
+                next_p=posts_np[i+1]
+            except:
+                next_p=''
+            break
+
+    
+        
+    context={
+    "posts":posts,
+    "next_p":next_p,
+    "prev_p":prev_p
+    }
     return render(request,"Blog\Blog-single.html",context)
     
 
 
 
 def http_test(request):
-    posts=datetime.now(timezone.utc)
+    posts=get_object_or_404(Post,id=1)
     context={"posts":posts}
     return render(request,"Blog\\test.html",context)
