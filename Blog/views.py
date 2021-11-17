9b85fa48b4f2a7d6d7d6f86332822b8622a7f1ae
myselfajp <myjaliliparsa@gmail.com>
2021-11-17 19:31:37 +0300

    Edited
    -edit views cods
    -made html files for include

--- Blog/views.py
+++ Blog/views.py
@@ -25,31 +25,17 @@ def http_blog_single(request,p1_id):
     posts.counted_views+=1
     posts.save()
 
-    utc_time_now=datetime.now(timezone.utc)
-    posts_np=Post.objects.exclude(published_date__gt= utc_time_now).filter(status=1)
-
-    for i in range(len(posts_np)):
-
-        if posts_np[i].id==posts.id:
-
-            try:
-                prev_p=posts_np[i-1]
-            except:
-                prev_p=''
-
-            try:
-                next_p=posts_np[i+1]
-            except:
-                next_p=''
-            break
-
     
-        
+    next_p=Post.objects.filter(id__gt=posts.id, status = 1, published_date__lte =datetime.now(timezone.utc)).order_by('id').first()
+    prev_p=Post.objects.filter(id__lt=posts.id, status = 1, published_date__lte =datetime.now(timezone.utc)).order_by('-id').first()
+
+
     context={
     "posts":posts,
     "next_p":next_p,
     "prev_p":prev_p
     }
+    
     return render(request,"Blog\Blog-single.html",context)
     
 
