93c199cde32b0c3f09b8c2e4f1d0040c85992aae
myselfajp <myjaliliparsa@gmail.com>
2021-12-11 10:22:57 +0300

    commen
    make comment model

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
-from Blog.models import Post,category,tag
+from Blog.models import Post,category,tag,comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
+
 class PostAdmin(SummernoteModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('title','author','status')
+    list_filter = ('status','author')
+    search_fields = ['title','content']
     summernote_fields = ('content',)
+
+class CommentAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('post','name','approved')
+    list_filter = ('post','approved')
+    search_fields = ['name','post']
 
 # Register your models here.
 admin.site.register(category)
 admin.site.register(tag)
+admin.site.register(comment,CommentAdmin)
 admin.site.register(Post,PostAdmin)
--- Blog/models.py
+++ Blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.fields import EmailField
 from django.urls import reverse
 
 # Create your models here.
@@ -27,7 +28,19 @@ class Post(models.Model):
     updated_date = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        return "{} - {} - {}".format(self.title,self.id,self.author)
+        return self.title
 
     def get_absolute_url(self):
         return reverse('Blog:blog-single',kwargs={'p1_id':self.id})
+
+class comment(models.Model):
+    post=models.ForeignKey(Post,on_delete=models.CASCADE)
+    name=models.CharField(max_length=255)
+    email= models.EmailField()
+    subject= models.CharField(max_length=255)
+    message= models.TextField()
+    created_date= models.DateTimeField(auto_now_add=True)
+    updated_date= models.DateTimeField(auto_now=True)
+    approved=models.BooleanField(default=False)
+    
+    
