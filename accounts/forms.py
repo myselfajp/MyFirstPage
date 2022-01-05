556d8772d7ed6f318bddfd5292ca2fa24e1a7176
myselfajp <myjaliliparsa@gmail.com>
2022-01-05 22:10:57 +0300

    update

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,5 @@
-from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django import forms
 
 class signup_form(UserCreationForm):
     class Meta:
