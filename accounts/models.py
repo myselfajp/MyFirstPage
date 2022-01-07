3b53ffcc487603f092acaec5481c4708fddc144e
myselfajp <myjaliliparsa@gmail.com>
2022-01-07 20:24:55 +0300

    made email unique
    for sign up email is unique now

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,6 @@
-from django.db import models
+from django.contrib.auth.models import User
+
+User._meta.get_field('email')._unique = True
+# User._meta.get_field('email')._error_messages= {'unique': ("A user with that username already exists.")}
 
 # Create your models here.
